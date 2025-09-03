cc4c799704ac93e88afe1d63f4bdadf291eaea84
Hracio11 <rehoracio2015@gmail.com>
2025-09-03 12:09:13 -0500

    v1.1

--- main.js
+++ main.js
@@ -18,3 +18,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   reveals.forEach(reveal => observer.observe(reveal));
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const section = document.querySelector(".resume-section");
+
+  function checkScroll() {
+    const sectionTop = section.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (sectionTop < windowHeight - 100) { 
+      section.classList.add("show");
+      window.removeEventListener("scroll", checkScroll); // solo una vez
+    }
+  }
+
+  window.addEventListener("scroll", checkScroll);
+});
